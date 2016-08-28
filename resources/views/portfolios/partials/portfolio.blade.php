b243e076141a5de43b94c0d74a6bef491de5cebd
ZigaStrgar <ziga_strgar@hotmail.com>
2016-08-28 09:32:19 +0200

    Added MAC in portfolio :)

--- resources/views/portfolios/partials/portfolio.blade.php
+++ resources/views/portfolios/partials/portfolio.blade.php
@@ -3,16 +3,24 @@
     <div class="col-xs-12 features">
         @each('portfolios.partials.feature', explode(",", $portfolio->features), 'feature')
     </div>
-    <div class="col-xs-12 col-sm-6 image">
-        <img src="{{ url("images/".$portfolio->imagePath) }}" alt="{{ $portfolio->title }}" class="img-responsive">
+    <div class="col-xs-12 col-sm-6 image mac">
+        <a href="#">
+            <img src="/assets/img/mac.png">
+            <div class="mac__content">
+                <img src="{{ url("images/".$portfolio->imagePath) }}" alt="{{ $portfolio->title }}"
+                     class="img-responsive">
+            </div>
+        </a>
     </div>
     <div class="col-xs-12 col-sm-6 content">
         <p>{{ $portfolio->content }}</p>
         @if(strlen($portfolio->link) > 0)
-            <a href="{{ $portfolio->link }}" target="_blank" rel="noopener noreferrer" class="btn btn--bg-transparent btn--border-blue">Visit live version</a>
+            <a href="{{ $portfolio->link }}" target="_blank" rel="noopener noreferrer"
+               class="btn btn--bg-transparent btn--border-blue">Visit live version</a>
         @endif
         @if(strlen($portfolio->git) > 0 && Auth::check() && Auth::user()->isAdmin())
-            <a href="{{ $portfolio->git }}" target="_blank" rel="noopener noreferrer" class="btn btn--bg-transparent btn--border-blue">Visit git repository</a>
+            <a href="{{ $portfolio->git }}" target="_blank" rel="noopener noreferrer"
+               class="btn btn--bg-transparent btn--border-blue"><span class="icon icon-code-fork"></span> Visit git repository</a>
         @endif
     </div>
 </section>
