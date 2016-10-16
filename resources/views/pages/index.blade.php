92038de1de91e8916357d033534292b78c8f2b9a
ZigaStrgar <ziga_strgar@hotmail.com>
2016-10-16 13:53:56 +0200

    Index text changed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,5 +7,5 @@
 @section('content')
     <h1 class="page-header text-center">So who am I?</h1>
     <p class="text-center h3"><strong>I am Žiga Strgar. Student at faculty of computer science and informatics in Ljubljana, Slovenia.</strong></p>
-    <p class="article-content mt20">Yeah well that statement above is 100% correct but I think I'm so much more than just that. </p>
+    <p class="article-content mt20">Yeah well that statement above is 100% correct but I think I'm so much more than <i>just</i> that. Yes my passion is web development and solving problems</p>
 @endsection
