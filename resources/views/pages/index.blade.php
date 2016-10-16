1424f29383c0876f5dcfca110714f1883a2ecafb
ZigaStrgar <ziga_strgar@hotmail.com>
2016-10-16 17:18:54 +0200

    Index text changed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,5 +7,7 @@
 @section('content')
     <h1 class="page-header text-center">So who am I?</h1>
     <p class="text-center h3"><strong>I am Žiga Strgar. Student at faculty of computer science and informatics in Ljubljana, Slovenia.</strong></p>
-    <p class="article-content mt20">Yeah well that statement above is 100% correct but I think I'm so much more than <i>just</i> that. Yes my passion is web development and solving problems</p>
+    <p class="article-content mt20">Yeah well that statement above is <b>100%</b> correct but I think I'm so much more than <i>"just"</i> that. Yes my passion is web development and solving problems but underneath all of that I'm a regular boy finding his relaxation in reading all kinds of books (usually non-fiction ones), TV shows and of course last but not least hanging out with my friends.</p>
+    <p class="article-content mt20">Web development, huuh? Started with it back in 2010 or 2011. While I was still in elementary school. Those websites weren't so cool & pretty but I got into it. They contained only HTML and CSS. And I was using NVU. Which means I didn't even bother with how the page was looking underneath all that visual representation inside the program.</p>
+    <p class="article-content mt20">And then here it is. Secondary school, project week and a course where I got introduced to PHP and MySQL.</p>
 @endsection
