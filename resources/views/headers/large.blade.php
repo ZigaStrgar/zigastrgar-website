0b661f4ccb91ea76c36863dec9079ff937cff155
Žiga Strgar <ziga_strgar@hotmail.com>
2018-01-04 16:21:44 +0100

    Update

--- resources/views/headers/large.blade.php
+++ resources/views/headers/large.blade.php
@@ -3,7 +3,7 @@
         <div class="profile-image main-profile-image"
              alt="Well this is me. Actually if you are seeing this text, that's probably not me :)"></div>
         <h1 class="text--white text-center">Žiga Strgar</h1>
-        <h3 class="text--white text-center thin-space">web developer, programmer, "otaku"</h3>
+        <h3 class="text--white text-center thin-space">web developer, programmer, explorer & life long learner</h3>
         <div class="text-center">
             <a href="{{ url('portfolio') }}" class="mt10 btn btn--bg-transparent btn--border-blue text--white">
                 See my work in action ...
--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -6,6 +6,7 @@
 
 @section('content')
     <section class="skill-set col-xs-12 center-block">
+        <p class="text-muted">Please note that I'm not some kind of magician or artisan, this are all the things & languages I worked in and by any means I don't call my self an artisan in any of them, but I always find a way to make it work.</p>
         @each('skills.partials.category', $categories, 'category')
     </section>
 @endsection
