b4bb619eb3f113eb97bfb6ad2b0f20f30243e489
ZigaStrgar <ziga.strgar@gmail.com>
2021-05-09 14:51:57 +0200

    Updated first page content

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,42 +5,54 @@
 @endsection
 
 @section('content')
-    <h1 class="page-header text-center">So, who am I?</h1>
-    <p class="text-center h3"><strong>I am Žiga Strgar. Freelancer, web enthusiast, loves to work on projects with deeper connotation.</strong></p>
-    <p class="article-content mt20">Yeah well that statement above is <b>100%</b> correct but I think I'm so much more
-        than <i>"just"</i> that. Yes my passion is web development and solving problems but underneath all of that I'm a
-        regular boy finding his relaxation in reading all kinds of books (usually non-fiction ones), TV shows and of
-        course last but not least hanging out with my friends.</p>
-    <p class="article-content mt20">Web development, huuh? Started with it back in 2010 or 2011. While I was still in
-        elementary school. Those websites weren't so cool & pretty but I got into it. They contained only HTML and CSS.
-        And I was using NVU. Which means I didn't even bother with how the page was looking underneath all that visual
-        representation inside the program.</p>
-    <p class="article-content mt20">And then here it is. Secondary school, project week and a course where I got
-        introduced to PHP and MySQL. I liked it and that's why I started with digging into PHP and SQL. Started working
+    <h1 class="page-header text-center">
+        Žiga Strgar
+    </h1>
+    <p class="text-center h3">
+        <strong>
+            Backend-oriented engineer focused on delivering highly performant and scalable solutions in Node.JS and Laravel.
+        </strong>
+    </p>
+    <p class="article-content mt20">
+        While the previous statement is correct, I would like for you to know more than
+        just the professional side of me. While my biggest passions include developing backend systems and solving problems.
+        Underneath all of that I'm a regular man finding his relaxation in reading all kinds of books (usually non-fiction ones),
+        tasting diffrent kinds of wines from all over the world, occasionally indulging in TV shows and of course last
+        but not least hanging out with my family, friends and lately my team-mates.
+    </p>
+    <p class="article-content mt20">
+        While my "professional" carrer started with developing websites all the way back in 2010 or 2011. At the time
+        I was still in the elementary school. Those pages weren't cool & pretty, but I've managed to get some knowledge
+        to make websites. And all that excitment led me to go to primary school for Computer Technitians, where
+        I've been introduced with C++ at first, and later on with PHP.
+    </p>
+    <p class="article-content mt20">
+        I liked PHP and that's why I started with digging into PHP and SQL. Started working
         on web pages for my friends, for me, my mom's company, etc. In 2 years I learned pretty much everything to make
-        a high quality web page not vulnerable to SQL Injection, XSS. At some point I wanted to re-create a CMS like
-        Wordpress but I quit when project was like 25% done :(</p>
-    <p class="article-content mt20">In late 2015 I discovered the PHP framework <a href="https://laravel.com"
-                                                                                   rel="noreferrer noopener"
-                                                                                   target="_blank">Laravel</a> at first
-        I couldn't wrap my head around it but once I found <a href="https://laracasts.com" rel="noreferrer noopener"
-                                                              target="_blank">Laracasts</a> it was like poetry.
+        a high quality web page not vulnerable to SQL Injection, XSS and simillar attacks. My last project with plan PHP,
+        MySQL, without any frameworks or simillar was my graduation project, website for selling used car parts from
+        junkyards and mechanics.
+    </p>
+    <p class="article-content mt20">
+        In late 2015 I discovered the PHP framework <a href="https://laravel.com" rel="noreferrer noopener" target="_blank">Laravel</a>
+        at first I couldn't wrap my head around it but once I found <a href="https://laracasts.com" rel="noreferrer noopener"
+                                                                       target="_blank">Laracasts</a> it was like poetry.
         Instantly enjoyed the development process and the whole structure how Laravel is built, booted and what kind of
         features it provides "out of the box". Also I've attended first LaraCon Online 2017 on March 8<sup>th</sup>.
     </p>
     <p class="article-content mt20">
-        Beside web development I'm also diving into the world of mobile development and desktop languages. In the mobile
-        world I'm working with Swift, for development of iOS applications. So far I just stayed with "playground"
-        application, nothing is published on AppStore so far. As desktop languages I'm referring to Java and C#, C# is
-        still from my high school years, but Java is more of a recent thing as we do all the assignments for faculty in
-        it. Lately I'm getting my hands dirty with Python in the world of Data Mining as part of the faculty curriculum.
+        During my faculty years I've used Python, Java, C, C#, and R. While in my free-time in those 3 years, I've also did some
+        developing with Swift. Python was used as part of the first programming language in the 1<sup>st</sup> year and
+        in the last year I've used Python and R for Data Mining projects and Decision & Recommender Systems curriculum.
     </p>
     <p class="article-content mt20">
-        Recently my professional life is covered with Node.JS development which is quite similar to Laravel but,
-        different :) And my perosnal life is filled with learning French. Oui, Française. Out of all languages.
+        Recently my carrer is covered with Node.JS development, and at the moment I enjoyed it much more than Laravel.
+        It's different but, a nice way of different. The world is a bit different with NoSQL databases and Fastify framework.
+    </p>
+    <p class="article-content mt20">
+        And my perosnal life is filled with learning French. Oui, Française. Out of all languages.
         It's my passion to learn French and someday live for a bit on the southern beaches of France.
-    </p>
-    <p class="article-content mt20">
-        On my programming bucket list are still this languages: SmallTalk, Go, Ruby on Rails.
+        Last but not least, it's filled with paying Flag Football as a defensive back at
+        <a rel="noopener" target="_blank" href="https://ljubljanafrogs.si/teams/ljubljana-frogs/players/ziga-strgar-25">Ljubljana Frogs</a>, which is also a club I co-founded in May 2020.
     </p>
 @endsection
